9043fae439e57c3079e4109ee015c58ece3b256b
Devil841 <102499062+Devil841@users.noreply.github.com>
2022-11-15 10:44:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ let domhour = document.getElementById("hour");
 let dommin = document.getElementById("minute");
 let domsec = document.getElementById("second");
 let ampm = document.getElementById("ampm");
-
 const print = () => {
    let time = new Date();
-   let hour = time.getHours();
    let minute = time.getMinutes();
    let seconds = time.getSeconds();
-   if (time.getHours() > 12) {
+   let hour = time.getHours();
+   if (hour > 12) {
       domhour.innerText = hour - 12;
       ampm.innerText = "PM";
    } else {
-      domhour.innerText = hours;
+      domhour.innerText = hour;
+      ampm.innerText = "AM";
    }
    dommin.innerText = minute;
    domsec.innerText = seconds;
+   console.log("Printed");
 };
 setInterval(() => print(), 1000);
